0e8854eefabcbf618b7b0f29bd20afc4ef77466e
camimisa <garciamisacamila@gmail.com>
2021-04-28 23:37:03 -0300

    first commit

---
+++ src/main/java/com/unla/grupoDos/GrupoDosApplication.java
@@ -0,0 +1,13 @@
+package com.unla.grupoDos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrupoDosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrupoDosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unla/grupoDos/GrupoDosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unla.grupoDos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrupoDosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

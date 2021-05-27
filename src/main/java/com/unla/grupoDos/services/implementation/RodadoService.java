65c971e1e9b2b4959fb4c13dee2f5ef69a29bf5c
camimisa <garciamisacamila@gmail.com>
2021-05-26 22:18:34 -0300

    abm rodado 1.0

---
+++ src/main/java/com/unla/grupoDos/services/implementation/RodadoService.java
@@ -0,0 +1,54 @@
+package com.unla.grupoDos.services.implementation;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import com.unla.grupoDos.converters.RodadoConverter;
+import com.unla.grupoDos.entities.Rodado;
+import com.unla.grupoDos.models.RodadoModel;
+import com.unla.grupoDos.repositories.IRodadoRepository;
+import com.unla.grupoDos.services.IRodadoService;
+@Service("RodadoService")
+public class RodadoService implements IRodadoService{
+	@Autowired
+	@Qualifier("RodadoRepository")
+	private IRodadoRepository RodadoRepository;
+	
+	@Autowired
+	@Qualifier("RodadoConverter")
+	private RodadoConverter RodadoConverter;	
+	
+	@Override
+	public List<Rodado> getAll() {
+		return RodadoRepository.findAll();
+	}
+
+	@Override
+	public RodadoModel findById(int id) {
+		return RodadoConverter.entidadAModelo(RodadoRepository.findByIdRodado(id));
+	}
+
+	@Override
+	public RodadoModel findByDominio(String dominio) {
+		return RodadoConverter.entidadAModelo(RodadoRepository.findByDominio(dominio));
+	}
+	@Override
+	public RodadoModel insertOrUpdate(RodadoModel RodadoModel) {
+		Rodado Rodado = RodadoRepository.save(RodadoConverter.modeloAEntidad(RodadoModel));
+		return RodadoConverter.entidadAModelo(Rodado);
+	}
+
+	@Override
+	public boolean remove(int id) {
+		try {
+			RodadoRepository.deleteById(id);
+			return true;
+		}
+		catch(Exception e) {
+			return false;
+		}
+	}
+}
